7cf7d14fb2c97e01976ff78c13341b3674422f6a
Robert Blutner <robert@blutner.de>
2017-12-17 21:07:13 +0100

    Initial

---
+++ Build.sh
@@ -0,0 +1,3 @@
+docker build -t blutner/ocomis-logging-elasticsearch:latest ./elasticsearch/
+docker build -t blutner/ocomis-logging-logstash:latest ./logstash/
+docker build -t blutner/ocomis-logging-kibana:latest ./kibana/
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ocomis-logging
+# Ocomis Logging
+
+Ocomis logging is based on the ELK stack, consisting of Elasticsearch, Logstash and Kibana.
